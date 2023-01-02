2deb23ca2bf1ae29ba1d6266725c4052a1c55835
Ron Javellana <Ronapj1991@gmail.com>
2023-01-01 19:39:36 -0500

    create interface and basic logic NOTE: bugs still present

---
+++ script.js
@@ -0,0 +1,118 @@
+const output = document.querySelector('.output');
+output.innerText = "";
+
+let operation = "";
+let userInput = 0;
+let value1 = 0;
+let value2 = 0;
+let final = 0;
+
+document.querySelector(".one").addEventListener('click', (e) => {  
+    userInput = 1;
+    display();
+})
+document.querySelector(".two").addEventListener('click', (e) => {
+    userInput = 2;
+    display();
+})
+document.querySelector(".three").addEventListener('click', (e) => {
+    userInput = 3;
+    display();
+})
+document.querySelector(".four").addEventListener('click', (e) => {
+    userInput = 4;
+    display();
+})
+document.querySelector(".five").addEventListener('click', (e) => {
+    userInput = 5;
+    display();
+})
+document.querySelector(".six").addEventListener('click', (e) => {
+    userInput = 6;
+    display();
+})
+document.querySelector(".seven").addEventListener('click', (e) => {
+    userInput = 7;
+    display();
+})
+document.querySelector(".eight").addEventListener('click', (e) => {
+    userInput = 8;
+    display();
+})
+document.querySelector(".nine").addEventListener('click', (e) => {
+    userInput = 9;
+    display();
+})
+document.querySelector(".clear").addEventListener('click', (e) => {
+    clearOutput();
+    value1 = 0;
+    value2 = 0;
+})
+document.querySelector(".dot").addEventListener('click', (e) => {
+    output.append(".");
+})
+document.querySelector(".plus").addEventListener('click', (e) => {
+    operation = "add";
+    value1 = parseInt(output.innerText);
+    clearOutput();
+})
+document.querySelector(".minus").addEventListener('click', (e) => {
+    operation = "subtract";
+    value1 = parseInt(output.innerText);
+    clearOutput();
+})
+document.querySelector(".times").addEventListener('click', (e) => {
+    operation = "multiply";
+    value1 = parseInt(output.innerText);
+    clearOutput();
+})
+document.querySelector(".divide").addEventListener('click', (e) => {
+    operation = "divide";
+    value1 = parseInt(output.innerText);
+    clearOutput();
+})
+document.querySelector(".equals").addEventListener('click', (e) => {
+     value2 = parseInt(output.innerText);
+     clearOutput();
+
+     switch (operation) {
+        case "add":
+            final = add(value1, value2);
+            break;
+        case "subtract":
+            final = subtract(value1, value2);
+            break;
+        case "divide":
+            final = divide(value1, value2);
+            break
+        case "multiply":
+            final = multiply(value1, value2);
+            break;
+     }
+
+     output.innerText = final;
+})
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function display(num=userInput.toString()) {
+    output.append(num.toString());
+}
+
+function clearOutput() {
+    output.innerText = "";
+}
